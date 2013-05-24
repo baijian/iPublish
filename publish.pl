b0f00075b6613da12fe99902ad9b907dc816737c
柏健 <bj@xiaocong.tv>
2013-05-24 20:27:24 +0800

    publish add ssh

--- publish.pl
+++ publish.pl
@@ -1,9 +1,12 @@
 #!/usr/bin/perl -w
 use strict;
+use Net::SSH::Perl;
 
-my @result = readpipe("ls -l /tmp");
-
-print @result;
-
-
+my $host = "192.168.1.1";
+my $user = "root";
+my $pass = "***";
+my $cmd = "ls /tmp";
+my $ssh = Net::SSH::Perl->new($host);
+$ssh->login($user, $pass);
+my($stdout, $stderr, $exit) = $ssh->cmd($cmd);
 
